2b5d539efa8124defa35d3c44a0e44de891482b5
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-08-28 10:22:23 +0100

    implimenter tache model and migration of create new tache

--- tache-service/src/main/java/tacheservice/model/Tache.java
+++ tache-service/src/main/java/tacheservice/model/Tache.java
@@ -1,4 +1,37 @@
 package tacheservice.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tacheservice.enums.Status;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "taches")
 public class Tache {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idTache;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "date_debut", nullable = false)
+    private LocalDate date_debut;
+
+    @Column(name = "date_fin", nullable = false)
+    private LocalDate date_fin;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "id_projet", nullable = false)
+    private int idProjet;
 }
