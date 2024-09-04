b4c765f93cdc98cf6bbd312dc38ee6447110be6e
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-04 16:24:11 +0100

    faire communication with feignClient

--- ressource-service/src/main/java/ressourceservice/RessourceServiceApplication.java
+++ ressource-service/src/main/java/ressourceservice/RessourceServiceApplication.java
@@ -2,8 +2,10 @@ package ressourceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RessourceServiceApplication {
 
     public static void main(String[] args) {
