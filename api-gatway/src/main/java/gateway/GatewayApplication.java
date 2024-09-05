11722418ea4babd2080f88f82e578c52125939c2
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-05 13:40:13 +0100

    secure my microservices

--- api-gatway/src/main/java/gateway/GatewayApplication.java
+++ api-gatway/src/main/java/gateway/GatewayApplication.java
@@ -3,6 +3,8 @@ package gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,11 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 }
